b1fbb0958d55636321cd9ab2af2ac65652e0094d
Jacky Lee <dr.jackylee@gmail.com>
2026-03-29 20:10:46 +0800

    feat: add processing metadata to items

--- src/data/items.js
+++ src/data/items.js
@@ -1,5 +1,7 @@
 import itemsData from './items.json';
 
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
 export const getItems = () => itemsData.items;
 export const getGems = () => itemsData.items.filter(item => item.category === 'Gem');
 export const getMinerals = () => itemsData.items.filter(item => item.category === 'Mineral');
@@ -7,3 +9,8 @@ export const getItemsByCategory = (category) => itemsData.items.filter(item => i
 export const getItemById = (id) => itemsData.items.find(item => item.id === id);
 export const getItemsByRarity = (rarity) => itemsData.items.filter(item => item.rarity === rarity);
 export const getItemCount = () => itemsData.items.length;
+export const getProcessableItems = (processType) => {
+  return getItems().filter(item => 
+    item.processing && item.processing[`can${capitalize(processType)}`]
+  );
+};
